a526a63dffb67be25a86287ab4457f9f618efdc1
Yulia Baranova <baranova.y@aetp.ru>
2017-01-27 18:48:15 +0300

    Third task changed to work with potentially eternal iterators more correcly

--- third_task/merge.py
+++ third_task/merge.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import heapq
 
 
-# Seems to be a cheat method to solve this task because of it`s just a one-line function,
-# but the point is that heapq.merge has a true iterative nature and means that it never reads any of the supplied
-# sequences all at once and already returns a fair generator.
-# Similar sorted(itertools.chain(*iterables)) pull the data into memory all at once because of sorted returns us
-# a simple list, so it seems useless to wrap it as generator because of potential problems with really long iterables
-# as an input sequence.
 def merge(*iterables):
-    """ Merge arbitrary number of sorted iterables. Uses heapq.
+    """ Merge arbitrary number of sorted iterables. Uses k-way merging algorithm.
 
     Args:
         *iterables (tuple of iterables): an arbitrary number of iterables each of sorted numbers,
         Empty iterables and no args allowed.
 
     Yields:
-        int: the next value of merged input iterables in  the sorted order
+        int: the next value of merged input iterables in the sorted order
 
     Examples:
         >>> list(merge((1, 2, 3), (0, 1), (5, 7)))
@@ -29,33 +22,21 @@ def merge(*iterables):
         >>> list(merge([1, 3], (0,)))
         [0, 1, 3]
     """
-    return heapq.merge(*iterables)
+    iterators = []
+    for iterator in map(iter, iterables):
+        try:
+            iterators.append([iterator.next(), iterator.next])
+        except StopIteration:
+            pass
 
-
-# Custom function doing the same but without heapq and itertools
-def custom_merge(*iterables):
-    """ Merge arbitrary number of sorted iterables. Uses k-way merging algorithm.
-
-    Args:
-        *iterables (tuple of iterables): an arbitrary number of iterables each of sorted numbers,
-        Empty iterables and no args allowed.
-
-    Yields:
-        int: the next value of merged input iterables in the sorted order
-
-    Examples:
-        >>> list(custom_merge((1, 2, 3), (0, 1), (5, 7)))
-        [0, 1, 1, 2, 3, 5, 7]
-
-        >>> list(custom_merge())
-        []
-
-        >>> list(custom_merge([1, 3], (0,)))
-        [0, 1, 3]
-    """
-    iterables = map(list, iterables)
-    while any(iterable for iterable in iterables):
-        ind, min_value = min(((ind, iterable[0]) for ind, iterable in enumerate(iterables) if iterable),
-                             key=lambda i: i[1])
-        iterables[ind].remove(min_value)
+    while iterators:
+        iterators.sort(key=lambda x: x[0])
+        min_value, next_value = iterators[0]
+        try:
+            iterators[0][0] = next_value()
+        except StopIteration:
+            iterators.pop(0)
         yield min_value
+
+
+#print list(merge((), (0, 1, 3, 10), (5, 7, 8, 9)))
--- third_task/merge_tests.py
+++ third_task/merge_tests.py
@@ -3,7 +3,7 @@
 import random
 import unittest
 
-from merge import custom_merge
+from merge import merge
 
 
 class MergeTests(unittest.TestCase):
@@ -14,19 +14,19 @@ class MergeTests(unittest.TestCase):
         """Are arbitrary number of iterators with random length processed as expected?"""
         iterators = [sorted(random.sample(range(10), random.randint(1, 7))) for _ in range(1, random.randint(5, 10))]
         expected_result = sorted(reduce(lambda x, y: x + y, iterators))
-        result = list(custom_merge(*iterators))
+        result = list(merge(*iterators))
         self.assertListEqual(result, expected_result, msg=self.message.format(expected_result, result))
 
     def test_empty_iterator(self):
         """Is passed empty iterator as one of the arguments processed correctly?"""
         tuple1, tuple2, tuple3 = (7, 10, 11, 11), (), (1, 1, 2, 3, 5)
         expected_result = sorted(tuple1 + tuple2 + tuple3)
-        result = list(custom_merge(tuple1, tuple2, tuple3))
+        result = list(merge(tuple1, tuple2, tuple3))
         self.assertListEqual(result, expected_result, msg=self.message.format(expected_result, result))
 
     def test_no_args(self):
         """Did we get an empty sequence in case when no arguments were passed?"""
-        result = list(custom_merge())
+        result = list(merge())
         self.assertListEqual(result, [], self.message.format([], result))
 
 
